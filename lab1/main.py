726bdc0d4614566eda0e9425a2fe16284b754c02
andshevliakov <shevlyakov.tlpi@gmail.com>
2023-02-27 12:34:02 +0200

    fix

--- lab1/main.py
+++ lab1/main.py
@@ -1,11 +1,12 @@
 from menu.menu import vector_model, boolean_model
 import sys
 
-def main():
+
+def main() -> None:
     if len(sys.argv) != 2:
         print("Incorrect number of arguments")
         sys.exit(1)
-    
+
     if sys.argv[1] == "--boolean":
         boolean_model()
     elif sys.argv[1] == "--vector":
@@ -13,6 +14,7 @@ def main():
     else:
         print("Incorrect argument")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
--- lab1/menu/menu.py
+++ lab1/menu/menu.py
@@ -3,12 +3,12 @@ from processor.vector_model import VectorSpaceModel
 from processor.boolean_model import BooleanModel
 
 
-def boolean_model():
+def boolean_model() -> None:
     terms_file = input("Input terms file location: ")
     documents_dir = input("Input documents directory location: ")
     try:
         boolean_mod = BooleanModel(terms_file=terms_file,
-                              documents_dir=documents_dir)
+                                   documents_dir=documents_dir)
     except FileNotFoundError:
         print("Docs or files not found.")
         return
@@ -16,23 +16,10 @@ def boolean_model():
         print("While processing docs and files, error occurred:", error)
         return
 
-    while True:
-        dnf_string = input("Input query in dnf format(q for exit): ")
-        if dnf_string.lower() == "quit":
-            break
-        elif dnf_string.lower() == "q":
-            break
-        else:
-            response = boolean_mod.make_query(dnf_string)
-            if not response:
-                print("Empty result")
-            iterator = 1
-            for key, val in response.items():
-                print(f"\n{iterator}. {key}\n{val}")
-                iterator += 1
+    provide_responses(boolean_mod)
 
 
-def vector_model():
+def vector_model() -> None:
     documents_dir = input("Input documents directory location: ")
     try:
         vector_mod = VectorSpaceModel(documents_dir=documents_dir)
@@ -43,6 +30,10 @@ def vector_model():
         print("While processing docs, error occurred:", error)
         return
 
+    provide_responses(vector_mod)
+
+
+def provide_responses(mod: Processor) -> None:
     while True:
         query_string = input("Input query (q for exit): ")
         if query_string.lower() == "quit":
@@ -50,7 +41,7 @@ def vector_model():
         elif query_string.lower() == "q":
             break
         else:
-            response = vector_mod.make_query(query_string)
+            response = mod.make_query(query_string)
             if not response:
                 print("Empty result")
             iterator = 1
